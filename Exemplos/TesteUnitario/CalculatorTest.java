c419bc81d6da2a0ed41a3d4e10fcb6fa8ccd5543
Marcos Luiz <lmarcos.luiz@gmail.com>
2023-08-15 09:50:51 -0300

    Exemplos

---
+++ Exemplos/TesteUnitario/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    
+    @Test
+    public void testAdd() {
+        Calculator calculator = new Calculator();
+        int result = calculator.add(2, 3);
+        assertEquals(5, result);
+    }
+}
